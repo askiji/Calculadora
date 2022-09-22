26a4d01c4ab487f3de154fc9551546076ee712e9
askiji <askijipro@gmail.com>
2022-09-22 18:19:12 +0200

    start

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+// TODO
 
         var  btnS : ImageButton
         btnS = findViewById(R.id.suma)
